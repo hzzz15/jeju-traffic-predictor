1f9f45771960d83e535d1480eebb873c92a02128
강희지 <shine_g_0517@naver.com>
2025-05-19 15:01:55 +0900

    fix: 배포용 FastAPI 주소로 API 호출 URL 수정

--- streamlit_app/utils.py
+++ streamlit_app/utils.py
@@ -1,7 +1,7 @@
 import requests
 
 def predict_speed_api(user_input: dict) -> float:
-    API_URL = "http://localhost:8000/predict"  # 배포 시 URL 수정
+    API_URL = "http://localhost:8000/predict"  
     try:
         response = requests.post(API_URL, json=user_input)
         response.raise_for_status()
